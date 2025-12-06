8528d7987fb0e4724e3c29829300e779e9d5602d
yamato-spirit <mamo831sk@gmail.com>
2025-12-06 21:26:48 +0900

    在庫・受注管理システム

---
+++ console-inventory-order-management-system/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("以下の4つから好きなメニュー番号を選択してください　(入力例：4）");
+        System.out.println("1：登録（商品の登録をします）");
+        System.out.println("2：在庫管理（在庫数を管理します）");
+        System.out.println("3：受注処理（商品の出荷を行います）");
+        System.out.println("4：売上レポートの確認・出力（商品の売上レポートの確認・ファイル出力をします）");
+    }
+}
